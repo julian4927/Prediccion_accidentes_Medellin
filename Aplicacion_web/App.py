443af29faa8855aab5e282eed7a88aafbaeb9124
santiagom26c <sanmejiacar@unal.edu.co>
2023-10-21 19:01:53 -0500

    se añaden filtros y funcion que crea el mapa

--- Aplicacion_web/App.py
+++ Aplicacion_web/App.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request
 from Scripts.funciones import *
 
 barrios = lista_valores_unicos('BARRIO')
@@ -9,15 +9,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    for barrio in barrios:
-        print(barrio)
     return render_template('inicio.html')
 
-@app.route("/historico")
+@app.route("/historico", methods=["GET", "POST"])
 def historico():
-    for tipos_accidente in tipos_accidentes:
-        print(tipos_accidente)
-    return render_template('historico.html')
+    # Configura las variables de selección con valores predeterminados
+    selected_fecha_inicio = request.form.get("fecha-inicio")
+    selected_fecha_fin = request.form.get("fecha-fin")
+    selected_barrio = request.form.get("barrio")
+    selected_tipo_accidente = request.form.get("tipo-accidente")
+    
+    mostrar_mapa = False  # Inicialmente, no se mostrará el mapa
+    
+    # Si se realiza una solicitud POST al presionar el botón "Filtrar datos", se generará el mapa aquí
+    if request.method == "POST" and request.form.get("filtrar"):
+        mapa = crear_mapa_todo()  # Reemplaza esto con la lógica para generar el mapa
+        mostrar_mapa = True
+
+    # Renderiza la plantilla historico.html
+    return render_template('historico.html', barrios=barrios, tipos_accidentes=tipos_accidentes,
+                           selected_fecha_inicio=selected_fecha_inicio, selected_fecha_fin=selected_fecha_fin,
+                           selected_barrio=selected_barrio, selected_tipo_accidente=selected_tipo_accidente,
+                           mostrar_mapa=mostrar_mapa)
+
 
 @app.route("/prediccion")
 def prediccion():
--- Aplicacion_web/Scripts/funciones.py
+++ Aplicacion_web/Scripts/funciones.py
@@ -1,9 +1,27 @@
 import pandas as pd
-
+import folium
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import geopandas as gpd
+from IPython.display import IFrame
 
 def lista_valores_unicos(columna):
     datos = pd.read_csv("data/data_limpia.csv",  sep=",", decimal=".")
     valores_distintos = datos[f'{columna}'].unique()
     return valores_distintos
+
+def crear_mapa_todo():
+  barrios_med = gpd.read_file('data/Barrio_Vereda.dbf')
+  mapa = folium.Map(width=1000, height=700, zoom_start=12, location=[6.27,-75.60])
+  folium.TileLayer('openstreetmap').add_to(mapa)
+  folium.GeoJson(data = barrios_med,
+               name = 'NOMBRE',
+               popup = folium.GeoJsonPopup(
+                  fields = ['CODIGO', 'NOMBRE'],
+                  aliases = ['Cod.', 'Barrio']
+               )
+               ).add_to(mapa)
+  return mapa
 
 
