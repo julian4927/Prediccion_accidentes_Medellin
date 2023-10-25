54a8e73bdcb6ccae4eceed2eaf0ce48ffc40c3da
santiagom26c <sanmejiacar@unal.edu.co>
2023-10-25 00:45:28 -0500

    cambio de estilo en mapa

--- Aplicacion_web/Scripts/funciones.py
+++ Aplicacion_web/Scripts/funciones.py
@@ -11,18 +11,24 @@ def lista_valores_unicos(columna):
     valores_distintos = datos[f'{columna}'].unique()
     return valores_distintos
 
+def style_function(feature):
+    return {
+        'color': 'black',  # Color de las líneas
+        'weight': 0.5  # Grosor de las líneas (ajusta este valor)
+    }
+
 def crear_mapa_todo():
   barrios_med = gpd.read_file('data/Barrios de Medellín/Barrio_Vereda.dbf')
   mapa = folium.Map(width=910, height=410, zoom_start=12, location=[6.27,-75.60])
   folium.TileLayer('openstreetmap').add_to(mapa)
-  folium.GeoJson(data = barrios_med,
+  folium.GeoJson(data = barrios_med,style_function=style_function,
                name = 'NOMBRE',
                popup = folium.GeoJsonPopup(
                   fields = ['CODIGO', 'NOMBRE'],
                   aliases = ['Cod.', 'Barrio']
                )
                ).add_to(mapa)
-  
+  #folium.GeoJson(data = barrios_med,style_function=style_function).add_to(mapa)
   mapa.save('templates/mapa_sincolores.html')
   return mapa
 
